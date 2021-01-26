645da4c0cd4943aef38a9e61c957d3113679bbdc
Kristin Risinger <krisinger1@gmail.com>
2021-01-26 13:30:44 -0600

    initial commit

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -3,7 +3,10 @@ package com.udacity
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import kotlin.properties.Delegates
 
@@ -16,18 +19,34 @@ class LoadingButton @JvmOverloads constructor(
     private val valueAnimator = ValueAnimator()
 
     private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+        when(new){
+            ButtonState.Clicked -> Log.i("LoadingButton", "Button clicked")
+            ButtonState.Loading -> Log.i("LoadingButton", "Button Loading")
+            ButtonState.Completed -> Log.i("LoadingButton", "Button completed")
 
+        }
     }
+
+    val paint = Paint().apply {
+        style= Paint.Style.FILL }
 
 
     init {
+        isClickable=true
 
     }
 
+    override fun performClick(): Boolean {
+        if(super.performClick()) return true
+        return true
+    }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-
+        paint.color= Color.GRAY
+        canvas?.drawRect(0f, 0f,widthSize.toFloat(), heightSize.toFloat(),paint)
+        paint.color=Color.BLUE
+        canvas?.drawRect(0f, 0f,(widthSize/2).toFloat(), (heightSize).toFloat(),paint)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
